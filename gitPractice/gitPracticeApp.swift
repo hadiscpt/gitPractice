1abf433daad17a1639a137e62c50cf456aeb15a7
hadiscpt <mhadisucipto21@gmail.com>
2023-05-26 15:41:12 +0700

    Initial Commit

---
+++ gitPractice/gitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitPracticeApp.swift
+//  gitPractice
+//
+//  Created by Muhammad Hadi Sucipto on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct gitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
